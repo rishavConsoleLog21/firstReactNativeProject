b89138e015934684e27d72fc0ba9c4ad3bdc635c
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-10-27 18:35:38 +0530

    Add Styling

---
+++ AppPro.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import {View, Text, StyleSheet, useColorScheme} from 'react-native';
+
+function AppPro(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.container}>
+      <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+        This is a {isDarkMode ? 'dark' : 'light'} mode
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'skyblue',
+  },
+  whiteText: {
+    color: '#FFFFFF',
+  },
+  darkText: {
+    color: '#000000',
+  },
+});
+
+export default AppPro;
